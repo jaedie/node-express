a81781dc961b2d7a4eaab6667f212c23855dbf35
jaedie <jamesleehw@gmail.com>
2021-05-27 18:06:33 +0900

    added public folder

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
-//this get function will run every time a user sends request from '/' a.k.a home page
-app.get("/", (req, res) => {
-  res.status(200).send("Home");
-});
+//setup static and middleware
+app.use(express.static("./public"));
 
-app.get("/about", (req, res) => {
-  res.status(200).send("About");
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+// });
 
 app.all("*", (req, res) => {
-  res.status(404).send("<h1>resource not found</h1>");
+  res.status(404).send("resource not found");
 });
 
 app.listen(5000, () => {
