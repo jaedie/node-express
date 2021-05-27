0b4b7871b412d07b85bef79536abc5685da6a631
jaedie <jamesleehw@gmail.com>
2021-05-27 17:48:36 +0900

    express server set

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Express Tutorial')
+const express = require("express");
+const app = express();
+
+//this get function will run every time a user sends request from '/' a.k.a home page
+app.get("/", (req, res) => {
+  res.status(200).send("Home");
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).send("About");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>resource not found</h1>");
+});
+
+app.listen(5000, () => {
+  console.log("server is listening on port 5000...");
+});
--- final/01-http-basics.js
+++ final/01-http-basics.js
@@ -1,26 +1,20 @@
-const http = require('http')
-
+const http = require("http");
 const server = http.createServer((req, res) => {
-  // console.log(req.method)
-  const url = req.url
-  // home page
-  if (url === '/') {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    res.write('<h1>home page</h1>')
-    res.end()
-  }
-  // about page
-  else if (url === '/about') {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    res.write('<h1>about page</h1>')
-    res.end()
-  }
-  // 404
-  else {
-    res.writeHead(404, { 'content-type': 'text/html' })
-    res.write('<h1>page not found</h1>')
-    res.end()
-  }
-})
+  const url = req.url;
 
-server.listen(5000)
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>home page</h1>");
+    res.end();
+  } else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>about page</h1>");
+    res.end();
+  } else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1>page not found</h1>");
+    res.end();
+  }
+});
+
+server.listen(5000);
