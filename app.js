a06eeef8c5cc66407c724876e575cb3bfe92a222
jaedie <jamesleehw@gmail.com>
2021-05-29 12:23:51 +0900

    query added

--- app.js
+++ app.js
@@ -1,18 +1,66 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const { products } = require("./data");
 
-//setup static and middleware
-app.use(express.static("./public"));
+app.get("/", (req, res) => {
+  res.send('<h1>Home Page</h1><a href="/api/products">products</a>');
+});
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
-// });
+app.get("/api/products", (req, res) => {
+  const essentialInfo = products.map((product) => {
+    const { id, image, price } = product;
+    return { id, image, price };
+  });
 
-app.all("*", (req, res) => {
-  res.status(404).send("resource not found");
+  res.send(`<a href="/api/products/${essentialInfo[0].id}">product ${essentialInfo[0].id}</a><br>
+  <a href="/api/products/${essentialInfo[1].id}">product ${essentialInfo[1].id}</a><br>
+  <a href="/api/products/${essentialInfo[2].id}">product ${essentialInfo[2].id}</a><br>
+  <a href="/api/products/${essentialInfo[3].id}">product ${essentialInfo[3].id}</a>`);
+});
+
+app.get("/api/products/:productID", (req, res) => {
+  const { productID } = req.params;
+
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
+
+  if (!singleProduct) {
+    return res.status(404).send("Product does not exist.");
+  }
+
+  res.json(singleProduct);
+});
+
+app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
+  console.log(req.params);
+  res.send("hello world");
+});
+
+app.get("/api/v1/query", (req, res) => {
+  console.log(req.query);
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
+
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+
+  if (sortedProducts.length < 1) {
+    // res.status(200).send("no products matching");
+    return res.status(200).json({ success: true, data: [] });
+  }
+
+  res.status(200).json(sortedProducts);
 });
 
 app.listen(5000, () => {
-  console.log("server is listening on port 5000...");
+  console.log("Server is listening on port 5000...");
 });
